81c5cf87f592131d0f8e13763f6e401521af3eea
Shaun C Read <shaun.c.read@gmail.com>
2017-11-14 15:41:28 +0000

    add task 2

---
+++ Task 2/run.py
@@ -0,0 +1,30 @@
+from __future__ import division
+import numpy as np
+from astropy import units as u
+import matplotlib.pyplot as plt
+
+from solution import simulate_light_curves
+
+
+observation_times = np.linspace(0, 500, 200) * u.day  # 200 observations over 500 days
+lag = 100*u.day
+mean_flux = 10*u.mJy
+continuum, emission_line, continuum_emission_line = simulate_light_curves(observation_times,
+                                                                          lag=lag,
+                                                                          mean_continuum_flux=mean_flux,
+                                                                          line_flux_ratio=2*u.mJy,  # line is twice as strong as the continuum
+                                                                          log_gp_scale=2,
+                                                                          log_gp_amplitude=0.4,
+                                                                          random_seed=0  # This is to get the same solution each time
+                                                                          )
+
+plt.plot(observation_times, continuum.to(mean_flux.unit), linestyle='--', marker='o', label='continuum')
+plt.plot(observation_times, emission_line.to(mean_flux.unit), linestyle='--', marker='o', label='emission_line')
+plt.plot(observation_times, continuum_emission_line.to(mean_flux.unit), linestyle='--', marker='o',
+         label='continuum + emission_line')
+for i in np.arange(0, plt.xlim()[1], lag.value):
+    plt.axvline(i, linestyle=':')
+plt.legend()
+plt.ylabel('flux / {}'.format(mean_flux.unit))
+plt.xlabel('time / {}'.format(observation_times.unit))
+plt.show()
---
+++ Task 2/solution.py
@@ -0,0 +1,25 @@
+from __future__ import division
+import numpy as np
+from astropy import units as u
+
+
+def simulate_light_curves(observation_times, lag, mean_continuum_flux, line_flux_ratio, log_gp_scale, log_gp_amplitude,
+                          random_seed):
+    """
+    Creates three QSO light curves (continuum, pure emission line, combined emission line and continuum)
+    :param observation_times:  astropy.Quantity (time)
+    :param lag: astropy.Quantity (time)
+    :param mean_continuum_flux:  astropy.Quantity (flux)
+    :param line_flux_ratio:  astropy.Quantity (flux)
+    :param log_gp_scale:  The scale of the gaussian process: float
+    :param log_gp_amplitude: The amplitude of the gaussian process: float
+    :param random_seed: int
+    :return:
+    """
+
+    np.random.seed(random_seed)  # makes output deterministic
+
+
+    ### your solution here
+
+    return [np.ones_like(observation_times.value) * i * u.mJy for i in range(3)]
