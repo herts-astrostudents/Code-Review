c9d6c5957e36b3a88db6cbfe2e186f0f582c4f0f
Shaun C Read <shaun.c.read@gmail.com>
2017-11-29 13:08:51 +0000

    streamline plotting

--- Task 3/solution.py
+++ Task 3/solution.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 def fib(levels):
 	result = []
 	for level in range(levels+1):	
@@ -11,17 +13,21 @@ def fib(levels):
 	return result[1:]
 
 
-def plot_fib(levels, padding=0):
-	if levels == []: return ''
-	max_char = max([len(str(n)) for n in max(levels, key=max)]) + padding
+def plot_fib(levels):
+	if levels == []: 
+		return ''
+	max_char = len(str(levels[-1][len(levels) // 2])) # middle of last row
 	formatted_levels = [' '.join(['{num: ^{fill}}'.format(num=num, fill=max_char) for num in level]) for level in levels]
 	max_len = len(formatted_levels[-1])
-	return '\n'.join(['{num: ^{pad}}'.format(num=num, pad=max_len) for num in formatted_levels])
+	return '\n'.join(['{level: ^{pad}}'.format(level=level, pad=max_len) for level in formatted_levels])
 
 
 if __name__ == '__main__':
-	result = fib(10)
+	import argparse
+	parser = argparse.ArgumentParser(description='Prints the Fibonacci pyramid to stdout')
+	parser.add_argument('nlevels', type=int)
+	args = parser.parse_args()
+	
+	result = fib(args.nlevels)
 	triangle = plot_fib(result)
-	with open('result.txt', 'w') as f:
-		f.write(triangle)
-		print(triangle)
+	print(triangle)
