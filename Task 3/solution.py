0de33a883517ce69bae6ecaaede502633262fa78
Shaun C Read <shaun.c.read@gmail.com>
2017-11-29 13:23:35 +0000

    optimise

--- Task 3/solution.py
+++ Task 3/solution.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
+import pytest
 
-def fib(levels):
+def fib(nlevels):
 	result = []
-	for level in range(levels+1):	
-		row = []
-		for index in range(level):
-			if index in (0, level-1):
-				row.append(1)
-			else:
-				row.append(sum(result[-1][index-1:index+1]))
+	for level in range(nlevels):	
+		row = [1]
+		for index in range(1, level):
+			row.append(sum(result[-1][index-1:index+1]))
+		if level > 0:
+			row.append(1)
 		result.append(row)
-	return result[1:]
+	return result
 
 
 def plot_fib(levels):
@@ -18,8 +18,21 @@ def plot_fib(levels):
 		return ''
 	max_char = len(str(levels[-1][len(levels) // 2])) # middle of last row
 	formatted_levels = [' '.join(['{num: ^{fill}}'.format(num=num, fill=max_char) for num in level]) for level in levels]
-	max_len = len(formatted_levels[-1])
+	max_len = len(formatted_levels[-1])  # string length of the last row
 	return '\n'.join(['{level: ^{pad}}'.format(level=level, pad=max_len) for level in formatted_levels])
+
+
+@pytest.mark.parametrize('nlevels', [0, 1, 11])
+def test_fib(nlevels):
+	"""
+	Test implementation by looking at the second diagonal, which should just be 1,2,3,4...
+	run with `pytest solution.py` 
+	"""
+	lists = fib(nlevels)
+	if nlevels == 0:
+		assert len(lists) == 0
+	else:
+		assert [level[-2] for level in lists[1:]] == list(range(1, nlevels))
 
 
 if __name__ == '__main__':
