e5e4540f159bce0ef995e13c7e05af41a9340372
Shaun C Read <shaun.c.read@gmail.com>
2018-04-09 16:24:55 +0100

    removed solution

---
+++ Task 7/solution.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from data import fetch_sdss_filter, fetch_vega_spectrum
+
+fig, ax = plt.subplots()
+
+#----------------------------------------------------------------------
+# Fetch and plot the Vega spectrum
+
+
+plt.show()
