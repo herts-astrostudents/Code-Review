e370d7f974f4d352164088e83cfb852b94adc973
Elena Gonzalez Egea <elenagonzalezegea@gmail.com>
2018-05-14 10:39:56 +0100

    add task 8 solution

--- Task 8/solution.py
+++ Task 8/solution.py
@@ -1,13 +1,28 @@
 
 def bootstrap_this(data):
+    import numpy as np
+    import random
+
+    #number of elements in the array
+    N = len(data)
+
+    #number of samples
+    samples = 1000
+
+    #distribution of mean values from each sample
+    means_list = []
+
+    for i in np.arange(samples):
+        new_sample = [] #here I will save the elements of the new sample
+        for j in np.arange(N):
+            index = random.randint(0,N-1) #generates a random integer between 1 and N
+            new_sample.append(data[index]) #adds a random value from the initial sample to the new sample
+        means_list.append(np.mean(new_sample)) #computes and save the mean of the new sample
+    
+    value = np.mean(means_list) #mean of the distribution of means
+    error = np.std(means_list) #error of the distribution of means    
+    means = np.array(means_list)
+
+    return value, error, means
 
 
-	# YOUR CODE HERE
-
-
-	# return:
-	#	mean value (float)
-	#	error estimate (float)
-	#	means of the each sample drawn (array of floats)
-
-	return value, error, means
