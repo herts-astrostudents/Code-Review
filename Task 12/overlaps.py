1e4d08fa261141d9bad261e3e5d29f726eedf3d8
Shaun C Read <shaun.c.read@gmail.com>
2018-08-08 11:42:19 +0100

    solution

--- Task 12/overlaps.py
+++ Task 12/overlaps.py
@@ -6,21 +6,27 @@ def overlap_python(primefile, happyfile):
     '''
     Return overlap between two lists using pure python.
     '''
-    
-    # return intersection
+    with open(primefile, 'r') as f:
+        primes = set(map(int, f.readlines()))
+    with open(happyfile, 'r') as f:
+        happys = set(map(int, f.readlines()))
+    return primes & happys
+
 
 
 def overlap_numpy(primefile, happyfile):
     '''
     Return overlap between two lists using numpy.
     '''
-
-    # return intersection
+    primes = np.loadtxt(primefile, dtype=int)
+    happys = np.loadtxt(happyfile, dtype=int)
+    return np.intersect1d(primes, happys, assume_unique=True)
 
 
 def overlap_pandas(primefile, happyfile):
     '''
     Return overlap between two lists using pandas.
     '''
-    
-    # return intersection
+    primes = pd.read_table(primefile, header=None)[0]
+    happys = pd.read_table(happyfile, header=None)[0]
+    return primes[primes.isin(happys)]
