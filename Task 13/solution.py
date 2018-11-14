d8ba7e5283a617203e5a5fde9384b6968591118c
Alex Jones <aj16acd@herts.ac.uk>
2018-11-14 17:16:15 +0000

    This is my first attempt at the solution.

--- Task 13/solution.py
+++ Task 13/solution.py
@@ -7,21 +7,29 @@ X = np.loadtxt('ra_dec.csv')
 
 # Target ra and dec for nearest neighbour search
 target_X = np.array([34.3509123, -5.328912])
+# target_X = np.array([3.4358269e+01, -5.4666713e+00])
 
 # Plot positions
-fig, ax = plt.subplots(figsize=(10,10))
+fig, ax = plt.subplots(figsize=(6,6))
 ax.plot(X[:,0], X[:,1], '.k')
 ax.set_ylabel('Dec / degrees')
 ax.set_xlabel('RA / degrees')
 ax.plot(target_X[0], target_X[1], 'xr', markersize=20)
-
 
 def nearest_neighbour(X, target_X):
     """
     Find the index of the nearest point in X to the target position
     """
 
-    # do your thing here
+    diffs = []
+
+    for i in range(0, len(X)):
+    	x_diff = abs(X[i,0] - target_X[0])
+    	y_diff = abs(X[i,1] - target_X[1])
+    	diff = np.sqrt(x_diff**2 + y_diff**2)
+    	diffs.append(diff)
+
+    closest_index = diffs.index(min(diffs))
 
     return closest_index
 
