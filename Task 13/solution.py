60203a373e606504a0ba66584f08dfe26e6784ac
Alex Jones <aj16acd@herts.ac.uk>
2018-11-15 10:20:06 +0000

    2nd commit, but haven't really achieved anything.

--- Task 13/solution.py
+++ Task 13/solution.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import random
 
 # Load ra and dec
 X = np.loadtxt('ra_dec.csv')
@@ -21,6 +22,10 @@ def nearest_neighbour(X, target_X):
     Find the index of the nearest point in X to the target position
     """
 
+    #################################################
+
+    # 'Solution 1 - search every data point until closest distance found'
+
     diffs = []
 
     for i in range(0, len(X)):
@@ -30,6 +35,28 @@ def nearest_neighbour(X, target_X):
     	diffs.append(diff)
 
     closest_index = diffs.index(min(diffs))
+
+	#################################################
+
+    # 'Solution 2'
+
+    # separation = 0.1
+    # N_points = len(X)
+    # closest_index = random.randint(0, N_points)
+
+    # for i in range(0, 10):
+    #     print "Iteration {}".format(i)
+    #     x_diff = abs(X[rand_index,0] - target_X[0])
+    #     y_diff = abs(X[rand_index,1] - target_X[1])
+    #     diff = np.sqrt(x_diff**2 + y_diff**2)
+    #     if diff < separation:
+    #         separation = 0.5 * separation
+    #         closest_index
+    #     else:
+    #         closest_index = random.randint(0, N_points)
+
+
+    #################################################
 
     return closest_index
 
