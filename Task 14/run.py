9ea14f323c20c228d96e710ad442097e641fd4cf
Max Timberhill <max.lisogorski@gmail.com>
2018-12-12 15:59:59 +0000

    task 14 solution

--- Task 14/run.py
+++ Task 14/run.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 from solution import read_data, plot_temperature_map
 
 dataset = read_data('data')
-plot_temperature_map(dataset, month=1)  # plot the temperature over x and y coordinates in February with an optional colourmap
-plot_temperature_map(dataset, month=8, cmap='viridis')
+plot_temperature_map(dataset.copy(), month=1)  # plot the temperature over x and y coordinates in February with an optional colourmap
+plot_temperature_map(dataset.copy(), month=8, cmap='viridis')
 plt.show()
--- Task 14/solution.py
+++ Task 14/solution.py
@@ -5,29 +5,51 @@ except ImportError:
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import datetime
 
 
 # this allows you to read timestamps properly from a filename
 # datatime64 objects store the month, day, second etc so you can access them
 def read_timestamp(fname):
-    return np.datetime64(os.path.basename(fname).split('_')[1][:-4])
+    timestamp_string = os.path.basename(fname).split('_')[1][:-7]
+    return datetime.datetime.strptime(timestamp_string, "%Y-%m-%dT%H:%M:%S.%f")
 
 
 def read_data(directory):
-    fnames = [directory+'/'+i for i in os.listdir(directory)]
+    fnames = [directory + '/' + i for i in os.listdir(directory)]
 
-    # do your stuff here
+    timestamps   = np.array([read_timestamp(fname) for fname in fnames if 'airtemp' in fname])
+    airtemps     = np.array([np.loadtxt(fname, delimiter=' ', unpack=True) for fname in fnames if 'airtemp'      in fname])
+    xcoordinates = np.array([np.loadtxt(fname, delimiter=' ', unpack=True) for fname in fnames if 'xcoordinates' in fname])
+    ycoordinates = np.array([np.loadtxt(fname, delimiter=' ', unpack=True) for fname in fnames if 'ycoordinates' in fname])
 
-    return dataset  # dataset might be an xarray.Dataset object or a clever structure of arrays 
+    return {
+        'timestamps'   : timestamps,
+        'airtemps'     : airtemps,
+        'xcoordinates' : xcoordinates,
+        'ycoordinates' : ycoordinates,
+    }  # dataset might be an xarray.Dataset object or a clever structure of arrays 
 
 
-def get_month_average_temp(dataset, month):
+def get_month_average_temp(dataset, month, average_function=np.mean):
     """
     dataset: xr.Dataset: dataset object containing the 'temp' variable over time and space
     month: int: Month of the year
     """
+    
+    mask = np.array([dt.month == month for dt in dataset['timestamps']])
+    dataset['timestamps']   = dataset['timestamps'  ][mask]
+    dataset['airtemps']     = dataset['airtemps'    ][mask]
+    dataset['xcoordinates'] = dataset['xcoordinates'][mask]
+    dataset['ycoordinates'] = dataset['ycoordinates'][mask]
 
-    # do your stuff here
+    print(month, mask)
+    averaged_dataset = {
+        'month'        : month, 
+        'airtemps'     : np.array(average_function(dataset['airtemps'],     axis=0)),
+        'xcoordinates' : np.array(average_function(dataset['xcoordinates'], axis=0)),
+        'ycoordinates' : np.array(average_function(dataset['ycoordinates'], axis=0)),
+    }
 
     return averaged_dataset  # averaged_dataset might also be an xarray.Dataset object or a clever structure of arrays 
 
@@ -36,7 +58,15 @@ def plot_data(dataset, **kwargs):
     """
     plots temperature over x and y for a single time (i.e. dataset should consist of 1 time)
     """
-    # do you stuff here
+    ax = kwargs.pop('ax')
+    ax.set_title("month = {}".format(dataset['month']))
+    
+    # PCOLORMESH
+    # cm = ax.pcolormesh(dataset['xcoordinates'], dataset['ycoordinates'], dataset['airtemps'], **kwargs)
+    # IMSHOW
+    cm = ax.imshow(dataset['airtemps'], aspect="equal", **kwargs)
+    
+    plt.colorbar(cm, ax=ax)
 
 
 def plot_temperature_map(dataset, month, **kwargs):
